ca1b65641ded38c4b66b4744f5030f8fd2a20e22
Joe Davidson <79029363+jdavidson85@users.noreply.github.com>
2023-03-26 11:07:37 -0500

    Add files via upload

--- Dictionaries_loops_program.py
+++ Dictionaries_loops_program.py
@@ -1,13 +1,29 @@
-days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-steps = []
-for i in range(len(days)):
-    s = int(input("Please enter the number of steps taken on " + days[i] + ": "))
-    steps.append(s)
-print("\n\n\nYou walked a total of " + str(sum(steps))+" steps during the week.\nThat was an average of "
-      + str(int((sum(steps)/len(steps)))))
-for i in range(len(steps)):
-    if steps[i] == max(steps):
-        print("The maximum steps you took were: " + str(steps[i]) + " on " + days[i])
-for i in range(len(steps)):
-    if steps[i] == min(steps):
-        print("The minimum steps you took were: " + str(steps[i]) + " on " + days[i])
+def main():
+    days_and_steps = {}
+    total = 0
+    maximum = 0
+    minimum = 200000000
+    print("You will be entering the date and number of steps taken for each ")
+    print("day in a week.")
+    for day in ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",):
+        steps = int(input("Please enter the number of steps taken on " + day + ": "))
+        days_and_steps[day] = steps
+        if steps > maximum:
+            maximum = steps
+        if steps < minimum:
+            minimum = steps
+        total += steps
+    average = total/7
+    print("\n\n\nYou walked a total of " + format(total, ',.0f') + "steps during the week")
+    print("That was an average of " + format(average, ',.0f'))
+    print("The maximum steps you took were: " + str(maximum) + " on ")
+    for d in days_and_steps:
+        if days_and_steps[d] == maximum:
+            print("\t" + d)
+    print("The minimum steps you took were: " + str(minimum) + " on ")
+    for d in days_and_steps:
+        if days_and_steps[d] == minimum:
+            print("\t" + d)
+
+
+main()
